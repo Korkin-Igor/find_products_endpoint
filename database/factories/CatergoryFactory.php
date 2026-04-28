97b5c473677ae3ad733265a64f510fa56caee70e
Korkin-Igor <korkinigorttit@gmail.com>
2026-04-28 12:11:44 +0700

    feat(db): set up and fill products and categories migrations, feat(modelm factory): create models and factories for categories and products migrations

---
+++ database/factories/CatergoryFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Catergory;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Catergory>
+ */
+class CatergoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
