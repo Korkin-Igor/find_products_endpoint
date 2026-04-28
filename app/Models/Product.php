b47fc4f19226418ce2072bb8155b75adc097a202
Korkin-Igor <korkinigorttit@gmail.com>
2026-04-28 12:30:45 +0700

    feat(models): fill Category.php and Product.php, set up relations

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,11 +2,27 @@
 
 namespace App\Models;
 
+use Database\Factories\ProductFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
-    /** @use HasFactory<\Database\Factories\ProductFactory> */
+    /** @use HasFactory<ProductFactory> */
     use HasFactory;
+
+    protected $table = 'products';
+
+    protected $fillable = [
+        'name',
+        'price',
+        'category_id',
+        'in_stock',
+    ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
