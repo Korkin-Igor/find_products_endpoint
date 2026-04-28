23a3052e22a67f7c5f9f1cf5fee7ba20db93f69f
Korkin-Igor <korkinigorttit@gmail.com>
2026-04-28 12:13:02 +0700

    fix(model, factory, migration): rename from "cartegory" to "category"

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,8 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Catergory extends Model
+class Category extends Model
 {
-    /** @use HasFactory<\Database\Factories\CatergoryFactory> */
+    /** @use HasFactory<\Database\Factories\CategoryFactory> */
     use HasFactory;
 }
