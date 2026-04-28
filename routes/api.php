a71ce4c477d7891cc4a7f551cf53d699c1003197
Korkin-Igor <korkinigorttit@gmail.com>
2026-04-28 12:05:08 +0700

    fix(routes): fix syntax error
    feat(controllers): create ProductController

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('products', ProductController::class, 'index');
+Route::get('products', [ProductController::class, 'index']);
